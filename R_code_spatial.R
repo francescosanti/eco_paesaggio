049278cffcc92b9bab7142fb713b38d586a36a40
francescosanti <62288920+francescosanti@users.noreply.github.com>
2020-05-01 15:55:56 +0200

    Update R_code_spatial.R

--- R_code_spatial.R
+++ R_code_spatial.R
@@ -15,7 +15,7 @@ plot(cadmium, lead, col = "red", pch = 19, cex = 2)
 # exercise: plot di copper e zinc con simbolo triangolo (pch=17) e colore verde
 plot(copper, zinc, col = "green", pch = 17, cex = 2, xlab = "rame", ylab = "zinco")
 
-# multiframe o multipanel
+# multiframe o multipanel (inserire più grafici in una finestra)
 par(mfrow = c(1, 2)) #divide lo schermo in due colonne e una riga
 plot(cadmium, lead, col = "red", pch = 19, cex = 2)
 plot(copper, zinc, col = "green", pch = 17, cex = 2, xlab = "rame", ylab = "zinco")
@@ -27,23 +27,26 @@ plot(copper, zinc, col = "green", pch = 17, cex = 2, xlab = "rame", ylab = "zinc
 
 # multiframe automatico
 install.packages("GGally")
-ggpairs(meuse[, 3:6]) #in diagonale distribuzione della frequenza dei dati della variabile, sotto la distribuzione dei punti di due variabili, sopra la correlazione fra due var
+library(GGally)
+ggpairs(meuse[, 3:6]) # in diagonale distribuzione della frequenza dei dati della variabile,
+                      # nella parte bassa la distribuzione dei punti di due variabili
+                      # nella parte alta la correlazione fra due variabili
 
 # spatial
 head(meuse)
 
-coordinates(meuse) = ~ x + y #colonne x e y sono da interpretare come coordinate
+coordinates(meuse) = ~ x + y # colonne x e y sono da interpretare come coordinate
 plot(meuse)
 
 # creare grafico spaziale con funzione spplot
-spplot(meuse, "zinc") #crea distribuzione spaziale di variabile zinc
+spplot(meuse, "zinc") # crea distribuzione spaziale di variabile zinc, i siti sono colorati in base al valore della variabile selezionata
 
 
-### 25/03/20 continua analisi spaziale
-names(meuse) #fa vedere i nomi delle colonne del dataframe
+### continuazione analisi spaziale  25/03/20
+names(meuse) # visualizzazione dei nomi delle colonne del dataframe
 spplot(meuse, "copper")
 
-bubble(meuse, "zinc") #dà rappresentazione spaziale analoga a quella di spplot, ma i punti, anzichè essere colorati diversamente a seconda del valore della variabile, hanno dimensione proporzionale al valore della variabile
+bubble(meuse, "zinc") # dà rappresentazione spaziale analoga a quella di spplot, i punti hanno dimensione proporzionale al valore della variabile
 
 bubble(meuse, "copper", col = "red")
 
