10a35c8c602f716b53a5b9e020b7f4becc550256
francescosanti <62288920+francescosanti@users.noreply.github.com>
2020-05-02 18:46:59 +0200

    Update R_code_point_pattern.R

--- R_code_point_pattern.R
+++ R_code_point_pattern.R
@@ -1,50 +1,84 @@
+### 
+
 setwd("C:/lab")
 
 # importazione dei dati
-covid <- read.table("covid_agg.csv",header=T)
-
+covid <- read.table("covid_agg.csv", header = T) # header=T per considerare la prima riga come intestazione dei campi
 head(covid)
 
-plot(covid$country,covid$cases)
+# plot con numero di casi per ogni paese
+plot(covid$country, covid$cases)
 # "las" argomento per posizionamento delle etichette sugli assi nel grafico, las sta per "labels"
-plot(covid$country,covid$cases,las=0) #las=0 etichette parallele agli assi
-plot(covid$country,covid$cases,las=1) #las=1 etichette orizzontali
-plot(covid$country,covid$cases,las=2) #las=2 perpendicolari agli assi
-plot(covid$country,covid$cases,las=3) #las=3 etichette verticali
+# cambiando il valore di "las" si può rendere il grafico più leggibile
+plot(covid$country, covid$cases, las = 0) # las=0 etichette parallele agli assi
+plot(covid$country, covid$cases, las = 1) # las=1 etichette orizzontali
+plot(covid$country, covid$cases, las = 2) # las=2 perpendicolari agli assi
+plot(covid$country, covid$cases, las = 3) # las=3 etichette verticali
 
+
+install.packages("ggplot2")
+# caricamento libreria e dataset
 library(ggplot2)
 data(mpg)
 head(mpg)
 
 
-# componenti: dati, aes (sta per "estetica"), geometria
-ggplot(mpg,aes(x=displ,y=hwy))+geom_point() 
-ggplot(mpg,aes(x=displ,y=hwy))+geom_line() 
+# per il comando 'ggplot' vanno specificate le componenti: dati, aes (sta per "estetica"), geometria
+ggplot(mpg, aes(x = displ, y = hwy)) + geom_point() # all'interno di aes si indicano i campi del dataset da utilizzare come coordinate
+                                                    # per disporre i punti spazialmente
 
-ggplot(covid,aes(x=lon,y=lat,size=cases)) + geom_point()
+ggplot(mpg, aes(x = displ, y = hwy)) + geom_line()  # con 'geom_point()', 'geom_line()', i siti vengono rappresentati rispettivamente come
+                                                    # punti o uniti da una linea; esistono anche altre possibilità, ad esempio 
+                                                    # 'geom_polygon()', in questo caso non funzionale
+
+
+# rappresentazione grafica del numero di casi di covid19 per ogni paese, con dimensione del punto proporzionale al valore dei casi (size=cases)
+ggplot(covid, aes(x = lon, y = lat, size = cases)) + geom_point()
 
 attach(covid)
-# con la funzione ppp si crea il point pattern
-covids <- ppp(lon, lat, c(-180,180), c(-90,90))
-d<-density(covids)
 
-plot(d) #viene creato un plot di densità dei punti, non prende in considerazione il numero di casi
-points(covids) #aggiunge i punti relativi ai vari paesi
+# installazione della libreria spatstat per analisi spaziali
+install.packages("spatsat")
+library(spatstat)
 
-# palette
-# scegliere colori da utilizzare nella mappa
-cl<- colorRampPalette(c('yellow','orange','red')) (100) #il numero tra parentesi indica il numero di gradazioni fra i vari colori, questa funzione è disponibile in alcune librerie, ad esempio spatstat
-plot(d,col=cl)
+# creazione del point pattern tramite la funzione ppp
+covids <- ppp(lon, lat, c(-180, 180), c(-90, 90))
+
+# con la funzione 'density' si va a stimare la diffusione del fenomeno analizzato, considerando la densità dei punti di partenza
+d <- density(covids)
+
+plot(d) # rappresentazione grafica; a seconda del colore di un pixel, ci si aspetta un numero più o meno alto di punti con casi accertati
+        # di covid19 (il numero di casi non è contemplato)
+points(covids) # aggiunta dei punti relativi ai vari paesi
+
+
+# per salvare gli oggetti attualmente caricati
+save.image("C:/lab/point_pattern.RData")
+
+# caricare il workspace precedentemente salvato
+setwd("C:/lab") #impostazione della working directory
+load("point_pattern.RData") # caricamento del file "rdata" desiderato
+
+ls() # mostra gli oggetti attualmente caricati
+
+
+# creazione di una palette
+# scegliere i colori da utilizzare nella mappa
+cl <- colorRampPalette(c('yellow', 'orange', 'red')) (100) # il numero tra parentesi indica il numero di gradazioni fra i vari colori
+                                                           # questa funzione è disponibile in alcune librerie, ad esempio in 'spatstat'
+plot(d, col = cl) # assegnare al grafico i colori della palette creata
 points(covids)
 
-library(rgdal) # la libreria 'gdal' permette a qualsiasi software di leggere dati geospaziali, in R si chiama rgdal
-coastlines<-readOGR("ne_10m_coastline.shp") #funzione per leggere file di tipo geografico
-plot(coastlines,add=T) # con add=T si aggiungono le linee all'ultimo grafico creato
+library(rgdal) # la libreria 'gdal' permette a qualsiasi software di leggere dati geospaziali, in R si chiama 'rgdal'
+coastlines <- readOGR("ne_10m_coastline.shp") # funzione per leggere file di tipo geografico, in questo caso uno shapefile
+
+# aggiunta al grafico di densità delle linee di coste mondiali
+plot(coastlines, add = T) # con add=T si aggiunge l'oggetto all'ultimo grafico creato
 
 # exercise: cambiare colori alla mappa di densità
-cl2<-colorRampPalette(c('white','blue','green','red','orange','yellow')) (150)
-plot(d,col=cl2)
-plot(coastlines,add=T,col="grey")
+cl2 <- colorRampPalette(c('white', 'blue', 'green', 'red', 'orange', 'yellow')) (150)
+plot(d, col = cl2)
+plot(coastlines, add = T, col = "grey")
 
 
 
@@ -53,6 +87,8 @@ library(spatstat)
 library(rgdal) # for the coastlines
 
 
+save.image("C:/lab/point_pattern.RData")
+
 setwd("C:/lab")
 load("point_pattern.RData")
 ls()
@@ -60,30 +96,62 @@ ls()
 cl5 <- colorRampPalette(c('cyan', 'purple', 'red')) (200) 
 plot(d, col=cl5, main="density")
 points(covids)
+library(rgdal)
 coastlines <- readOGR("ne_10m_coastline.shp")
-plot(coastlines, add=T)
+plot(coastlines, add = T)
 
 
 # interpolazione
 
 head(covid)
-marks(covids)<-covid$cases #funzione marks è in spatstat
+marks(covids) <- covid$cases # funzione 'marks' è in 'spatstat'
+# 'marks' associa i valori del campo scelto ai punti di un point pattern (di default il primo del campo al primo del pp, il secondo al secondo ecc)
+s <- Smooth(covids) # interpolazione spaziale del numero di casi di covid19 nei vari paesi, a partire dal dataset 'covid'
+plot(s) # analogamente alla mappa di densità, il colore di un pixel rappresenta il valore atteso della variabile scelta
+ 
+plot(coastlines, add = T)
+points(covids)
+
+
+
+##### mappa finale
+par(mfrow = c(2, 1)) # la schermata grafica viene suddivisa in due righe e una colonna
+                     # in questo modo si possono comparare la mappa di densità e di interpolazione del numero di casi
+ 
+# densità
+cl5 <- colorRampPalette(c('cyan', 'purple', 'red')) (200) 
+plot(d, col = cl5, main="density")
+points(covids)
+coastlines <- readOGR("ne_10m_coastline.shp")
+plot(coastlines, add = T)
+
+# interpolazione del numero di casi
+cl5 <- colorRampPalette(c('cyan', 'purple', 'red')) (200) 
+plot(s, col = cl5, main = "estimate of cases")
+points(covids)
+coastlines <- readOGR("ne_10m_coastline.shp")
+plot(coastlines, add = T)
+
+
 
 
 ###### San Marino
- # setwd("C:/lab")
- # library(spatstat)
+# setwd("C:/lab")
+# library(spatstat)
  
- head(Tesi)
- attach(Tesi)
+load("Tesi.RData")
+ls()
+
+head(Tesi)
+attach(Tesi)
  
- summary(Tesi)
+summary(Tesi)
  
- # x varia da 12.42 a 12.46
- # y varia da 43.91 a 43.94
- # point pattern: x,y,c(xmin,xmax),c(ymin,ymax)
- Tesippp<-ppp(Longitude,Latitude,c(12.41,12.47),c(43.9,43.95))
+# x varia da 12.42 a 12.46
+# y varia da 43.91 a 43.94
+# point pattern: x,y,c(xmin,xmax),c(ymin,ymax)
+Tesippp <- ppp(Longitude, Latitude, c(12.41, 12.47), c(43.9, 43.95))
  
- dT<-density(Tesippp)
- plot(dT)
- points(Tesippp,col="green")
+dT <- density(Tesippp)
+plot(dT)
+points(Tesippp, col = "green")
