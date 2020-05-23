085877f6ff2303ff4c9a3e70dbf2b163957bc870
francescosanti <62288920+francescosanti@users.noreply.github.com>
2020-05-23 14:49:02 +0200

    Update R_code_multitemp_NO2.R

--- R_code_multitemp_NO2.R
+++ R_code_multitemp_NO2.R
@@ -27,8 +27,8 @@ EN13 <- raster("EN_0013.png")
 
 # altra possibilità, con ciclo
 for(a in 1:13){
-  assign(paste("EN",rep("0",1-as.integer(log10(a))),a,sep=""),
-         raster(paste("EN_00",rep("0",1-as.integer(log10(a))),a,".png",sep="")))
+  assign(paste("EN", rep("0", 1 - as.integer(log10(a))), a, sep = ""),
+         raster(paste("EN_00", rep("0", 1 - as.integer(log10(a))), a, ".png", sep = "")))
 }
 # comando specifico per questo esercizio, non direttamente generalizzabile => un po' limitato
 
@@ -38,10 +38,10 @@ library(raster)
 # file da importare inseriti in un'unica cartella
 setwd("~/lab/esa_no2")
 
-# file con estensione .png inseriti in un oggetto
+# file con estensione .png inseriti in un oggetto lista
 rlist <- list.files(pattern = ".png", full.names = T)
 
-# importazione vera e propria dei raster
+# importazione vera e propria dei file raster
 list_rast <- lapply(rlist, raster) # con lapply si applica un comando a tutti gli oggetti di una lista
 
 # importazione può essere effettuata anche con ciclo for
@@ -53,11 +53,12 @@ for(i in 1:length(rlist)){
 
 
 ########
-cl <- colorRampPalette(c('red','orange','yellow'))(100) #
-plot(EN01,col=cl)
+cl <- colorRampPalette(c('red', 'orange', 'yellow'))(100)
+plot(EN01, col = cl)
 
-plot(EN13, col=cl)
+plot(EN13, col = cl)
 
+# confronto fra la prima e l'ultima immagine (in senso temporale) disponibile delle registrazioni relative all'emissione di NO2 in atmosfera 
 par(mfrow=c(1,2))
 plot(EN01, col=cl)
 plot(EN13, col=cl)
@@ -65,26 +66,26 @@ plot(EN13, col=cl)
 
 # difference
 difno2 <- EN13 - EN01
-cldif <- colorRampPalette(c('blue','black','yellow'))(100) #
-plot(difno2, col=cldif)
-# in questo modo risaltano nettamente i punti in cui l'emissione di NO2 è diminuita (giallo???????) e in cui è aumentata (blu)
+cldif <- colorRampPalette(c('blue', 'black', 'yellow'))(100)
+plot(difno2, col = cldif)
+# in questo modo risaltano nettamente i punti in cui l'emissione di NO2 è diminuita (blu), aumentata (giallo) o rimasta stabile (nero)
 
 
 # multiframe
-par(mfrow=c(4,4))
-plot(EN01, col=cl)
-plot(EN02, col=cl)
-plot(EN03, col=cl)
-plot(EN04, col=cl)
-plot(EN05, col=cl)
-plot(EN06, col=cl)
-plot(EN07, col=cl)
-plot(EN08, col=cl)
-plot(EN09, col=cl)
-plot(EN10, col=cl)
-plot(EN11, col=cl)
-plot(EN12, col=cl)
-plot(EN13, col=cl)
+par(mfrow = c(4, 4))
+plot(EN01, col = cl)
+plot(EN02, col = cl)
+plot(EN03, col = cl)
+plot(EN04, col = cl)
+plot(EN05, col = cl)
+plot(EN06, col = cl)
+plot(EN07, col = cl)
+plot(EN08, col = cl)
+plot(EN09, col = cl)
+plot(EN10, col = cl)
+plot(EN11, col = cl)
+plot(EN12, col = cl)
+plot(EN13, col = cl)
 
 
 ### day 2
@@ -95,7 +96,7 @@ ls()
 
 
 setwd("C:/lab/esa_no2")
-rlist <- list.files(pattern=".png")
+rlist <- list.files(pattern = ".png")
 rlist
 
 listafinale <- lapply(rlist, raster) # lapply sta per "list apply"
@@ -105,18 +106,20 @@ listafinale
 EN <- stack(listafinale) # crea un oggetto compatto con tutte le immagini, in questo modo si possono plottare tutte con un solo comando
 
 difEN <- EN$EN_0013 - EN$EN_0001
-cld <- colorRampPalette(c('blue','white','red'))(100) # 
-plot(difEN, col=cld)
+cld <- colorRampPalette(c('blue', 'white', 'red'))(100)
+plot(difEN, col = cld) # rappresentazione analoga a quella già plottata precedentemente, ma con colori diversi
 
-cl <- colorRampPalette(c('red','orange','yellow'))(100) #
-plot(EN, col=cl)
+cl <- colorRampPalette(c('red', 'orange', 'yellow'))(100)
+plot(EN, col = cl)
 
 
-boxplot(EN, horizontal=T,  # le barre dei boxplot vengono poste orizzontalmente
-        outline=F,         # vengono eliminati gli outliners, i punti che stanno all'esterno dell'intervallo primo-terzo quartile
-        axes=T)            # axes=T è di default, con axes=F non vengono rappresentati gli assi nel plot
+boxplot(EN, horizontal = T,  # le barre dei boxplot vengono poste orizzontalmente
+        outline = F,         # vengono eliminati gli outliners, i punti che stanno all'esterno dell'intervallo primo-terzo quartile
+        axes = T,            # axes=T è di default, con axes=F non vengono rappresentati gli assi nel plot
+        las = 1)
   
-  # a livello continentale, non cambiano molto i valori medi di NO2, ciò che cambia sono i valori max, che calano dai primi frame agli ultimi
+# a livello continentale, non cambiano molto i valori medi di NO2
+# ciò che cambia sono i valori max, che calano dai primi frame agli ultimi
   
   
   
