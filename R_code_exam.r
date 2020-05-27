fd253037cbac4425fabc375fe183fdf27c417e72
francescosanti <62288920+francescosanti@users.noreply.github.com>
2020-05-27 22:05:14 +0200

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -118,47 +118,47 @@ plot(cadmium, lead, col = "red", pch = 19, cex = 2)
 # exercise: plot di copper e zinc con simbolo triangolo (pch=17) e colore verde
 plot(copper, zinc, col = "green", pch = 17, cex = 2, xlab = "rame", ylab = "zinco")
 
-# multiframe o multipanel (inserire più grafici in una finestra)
+# FS  multiframe o multipanel (inserire più grafici in una finestra)
 par(mfrow = c(1, 2)) # FS  divide lo schermo in due colonne e una riga
 plot(cadmium, lead, col = "red", pch = 19, cex = 2)
 plot(copper, zinc, col = "green", pch = 17, cex = 2, xlab = "rame", ylab = "zinco")
 
-# inversione di grafici riga/colonna in colonna/riga
+# FS  inversione di grafici riga/colonna in colonna/riga
 par(mfrow = c(2, 1))
 plot(cadmium, lead, col = "red", pch = 19, cex = 2)
 plot(copper, zinc, col = "green", pch = 17, cex = 2, xlab = "rame", ylab = "zinco")
 
-# multiframe automatico
+# FS  multiframe automatico
 install.packages("GGally")
 library(GGally)
-ggpairs(meuse[, 3:6]) # in diagonale distribuzione della frequenza dei dati della variabile,
-                      # nella parte bassa la distribuzione dei punti di due variabili
-                      # nella parte alta la correlazione fra due variabili
+ggpairs(meuse[, 3:6]) # FS  in diagonale distribuzione della frequenza dei dati della variabile,
+                      # FS  nella parte bassa la distribuzione dei punti di due variabili
+                      # FS  nella parte alta la correlazione fra due variabili
 
 # spatial
 library(sp)
 head(meuse)
 
-coordinates(meuse) = ~ x + y # colonne x e y sono da interpretare come coordinate
+coordinates(meuse) = ~ x + y # FS  far capire al software che le colonne x e y sono da interpretare come coordinate
 plot(meuse)
 
-# creare grafico spaziale con funzione spplot
-spplot(meuse, "zinc") # crea distribuzione spaziale di variabile zinc,
-                      # i siti sono colorati in base al valore della variabile selezionata
+# FS  creare grafico spaziale con funzione spplot
+spplot(meuse, "zinc") # FS  crea distribuzione spaziale di variabile zinc,
+                      # FS  i siti sono colorati in base al valore della variabile selezionata
 
 ### continuazione analisi spaziale  25/03/20
-names(meuse) # visualizzazione dei nomi delle colonne del dataframe
+names(meuse) # FS  visualizzazione dei nomi delle colonne del dataframe
 spplot(meuse, "copper")
 
-bubble(meuse, "zinc") # dà rappresentazione spaziale analoga a quella di spplot,
-                      # i punti hanno dimensione proporzionale al valore della variabile
+bubble(meuse, "zinc") # FS  dà rappresentazione spaziale analoga a quella di spplot,
+                      # FS  i punti hanno dimensione proporzionale al valore della variabile
 
 bubble(meuse, "copper", col = "red")
 
 # formainiferi (Sofia), carbon capture (Marco)
 # creazione di array
-foram <- c(12, 20, 35, 55, 67, 80) # la lettera c sta per il termine inglese 'concatenate'
-carbon <- c(5, 15, 30, 70, 85, 99) # c() si utilizza quando si deve specificare più di un oggetto
+foram <- c(12, 20, 35, 55, 67, 80) # FS  la lettera c sta per il termine inglese 'concatenate'
+carbon <- c(5, 15, 30, 70, 85, 99) # FS  c() si utilizza quando si deve specificare più di un oggetto
 plot(foram, carbon, pch = 19, col = "green", cex = 2)
 
 
@@ -170,84 +170,86 @@ plot(foram, carbon, pch = 19, col = "green", cex = 2)
 
 setwd("C:/lab")
 
-# importazione dei dati
+# FS  importazione dei dati
 covid <- read.table("covid_agg.csv", header = T) # header=T per considerare la prima riga come intestazione dei campi
 head(covid)
 
-# plot con numero di casi per ogni paese
+# FS  plot con numero di casi per ogni paese
 plot(covid$country, covid$cases)
-# "las" argomento per posizionamento delle etichette sugli assi nel grafico, las sta per "labels"
-# cambiando il valore di "las" si può rendere il grafico più leggibile
-plot(covid$country, covid$cases, las = 0) # las=0 etichette parallele agli assi
-plot(covid$country, covid$cases, las = 1) # las=1 etichette orizzontali
-plot(covid$country, covid$cases, las = 2) # las=2 perpendicolari agli assi
-plot(covid$country, covid$cases, las = 3) # las=3 etichette verticali
+# FS  "las" argomento per posizionamento delle etichette sugli assi nel grafico, las sta per "labels"
+# FS  cambiando il valore di "las" si può rendere il grafico più leggibile
+plot(covid$country, covid$cases, las = 0) # FS  las=0 etichette parallele agli assi
+plot(covid$country, covid$cases, las = 1) # FS  las=1 etichette orizzontali
+plot(covid$country, covid$cases, las = 2) # FS  las=2 perpendicolari agli assi
+plot(covid$country, covid$cases, las = 3) # FS  las=3 etichette verticali
 
 
 install.packages("ggplot2")
-# caricamento libreria e dataset
+# FS  caricamento libreria e dataset
 library(ggplot2)
 data(mpg)
 head(mpg)
 
 
-# per il comando 'ggplot' vanno specificate le componenti: dati, aes (sta per "estetica"), geometria
-ggplot(mpg, aes(x = displ, y = hwy)) + geom_point() # all'interno di aes si indicano i campi del dataset da utilizzare come coordinate
-                                                    # per disporre i punti spazialmente
+# FS  per il comando 'ggplot' vanno specificate le componenti: dati, aes (sta per "estetica"), geometria
+ggplot(mpg, aes(x = displ, y = hwy)) + geom_point() # FS  all'interno di aes si indicano i campi del dataset da utilizzare come coordinate
+                                                    # FS  per disporre i punti spazialmente
 
-ggplot(mpg, aes(x = displ, y = hwy)) + geom_line()  # con 'geom_point()', 'geom_line()', i siti vengono rappresentati rispettivamente come
-                                                    # punti o uniti da una linea; esistono anche altre possibilità, ad esempio 
-                                                    # 'geom_polygon()', in questo caso non funzionale
+ggplot(mpg, aes(x = displ, y = hwy)) + geom_line()  # FS  con 'geom_point()', 'geom_line()', i siti vengono rappresentati rispettivamente come
+                                                    # FS  punti o uniti da una linea; esistono anche altre possibilità, ad esempio 
+                                                    # FS  'geom_polygon()', in questo caso non funzionale
 
 
-# rappresentazione grafica del numero di casi di covid19 per ogni paese, con dimensione del punto proporzionale al valore dei casi (size=cases)
+# FS  rappresentazione grafica del numero di casi di covid19 per ogni paese, con dimensione del punto proporzionale al valore dei casi (size=cases)
 ggplot(covid, aes(x = lon, y = lat, size = cases)) + geom_point()
 
 attach(covid)
 
-# installazione della libreria spatstat per analisi spaziali
+# FS  installazione della libreria spatstat per analisi spaziali
 install.packages("spatsat")
 library(spatstat)
 
-# creazione del point pattern tramite la funzione ppp
+# FS  creazione del point pattern tramite la funzione ppp
 covids <- ppp(lon, lat, c(-180, 180), c(-90, 90))
 
-# con la funzione 'density' si va a stimare la diffusione del fenomeno analizzato, considerando la densità dei punti di partenza
+# FS  con la funzione 'density' si va a stimare la diffusione del fenomeno analizzato, considerando la densità dei punti di partenza
 d <- density(covids)
 
-plot(d) # rappresentazione grafica; a seconda del colore di un pixel, ci si aspetta un numero più o meno alto di punti con casi accertati
-        # di covid19 (il numero di casi non è contemplato)
-points(covids) # aggiunta dei punti relativi ai vari paesi
+plot(d) # FS  rappresentazione grafica; a seconda del colore di un pixel, ci si aspetta un numero più o meno alto di punti con casi accertati
+        # FS  di covid19 (il numero di casi non è contemplato)
+points(covids) # FS  aggiunta dei punti relativi ai vari paesi
 
 
-# per salvare gli oggetti attualmente caricati
+# FS  per salvare tutti gli oggetti attualmente caricati
 save.image("C:/lab/point_pattern.RData")
-
-# caricare il workspace precedentemente salvato
-setwd("C:/lab") #impostazione della working directory
-load("point_pattern.RData") # caricamento del file "rdata" desiderato
-
-ls() # mostra gli oggetti attualmente caricati
+# FS oppure
+save(oggetto1, oggetto2, file = "nome_file.rdata") # FS  per salvare alcuni oggetti fra quelli esistenti
 
 
-# creazione di una palette
-# scegliere i colori da utilizzare nella mappa
-cl <- colorRampPalette(c('yellow', 'orange', 'red')) (100) # il numero tra parentesi indica il numero di gradazioni fra i vari colori
-                                                           # questa funzione è disponibile in alcune librerie, ad esempio in 'spatstat'
-plot(d, col = cl) # assegnare al grafico i colori della palette creata
+# FS  caricare il workspace precedentemente salvato
+setwd("C:/lab") # FS  impostazione della working directory
+load("point_pattern.RData") # FS  caricamento del file ".rdata" desiderato
+
+ls() # FS  mostra gli oggetti attualmente caricati
+
+
+# FS  creazione di una palette
+# FS  scegliere i colori da utilizzare nella mappa
+cl <- colorRampPalette(c('yellow', 'orange', 'red')) (100) # FS  il numero tra parentesi indica il numero di gradazioni fra i vari colori
+                                                           # FS  questa funzione è disponibile in alcune librerie, ad esempio in 'spatstat'
+plot(d, col = cl) # FS  assegnare al grafico i colori della palette creata
 points(covids)
 
-library(rgdal) # la libreria 'gdal' permette a qualsiasi software di leggere dati geospaziali, in R si chiama 'rgdal'
-coastlines <- readOGR("ne_10m_coastline.shp") # funzione per leggere file di tipo geografico, in questo caso uno shapefile
+library(rgdal) # FS  la libreria 'gdal' permette a qualsiasi software di leggere dati geospaziali, in R si chiama 'rgdal'
+coastlines <- readOGR("ne_10m_coastline.shp") # FS  funzione per leggere file di tipo geografico, in questo caso uno shapefile
 
-# aggiunta al grafico di densità delle linee di coste mondiali
-plot(coastlines, add = T) # con add=T si aggiunge l'oggetto all'ultimo grafico creato
+# FS  aggiunta al grafico di densità delle linee di coste mondiali
+plot(coastlines, add = T) # FS  con add=T si aggiunge l'oggetto all'ultimo grafico creato
 
 # exercise: cambiare colori alla mappa di densità
 cl2 <- colorRampPalette(c('white', 'blue', 'green', 'red', 'orange', 'yellow')) (150)
 plot(d, col = cl2)
 plot(coastlines, add = T, col = "grey")
-
 
 
 # exercise: creare mappa di densità con dati covid
@@ -272,11 +274,12 @@ plot(coastlines, add = T)
 # interpolazione
 
 head(covid)
-marks(covids) <- covid$cases # funzione 'marks' è in 'spatstat'
-# 'marks' associa i valori del campo scelto ai punti di un point pattern (di default il primo del campo al primo del pp, il secondo al secondo ecc)
-s <- Smooth(covids) # interpolazione spaziale del numero di casi di covid19 nel mondo, a partire dal dataset 'covid'
-# 'Smooth' esegue l'interpolazione
-plot(s) # analogamente alla mappa di densità, il colore di un pixel rappresenta il valore atteso della variabile scelta
+marks(covids) <- covid$cases # FS  funzione 'marks' è in 'spatstat'
+# FS  'marks' associa i valori del campo scelto ai punti di un point pattern (di default il primo del campo al primo del pp,
+# FS  il secondo al secondo ecc)
+s <- Smooth(covids) # FS  interpolazione spaziale del numero di casi di covid19 nel mondo, a partire dal dataset 'covid'
+# FS  'Smooth' esegue l'interpolazione
+plot(s) # FS  analogamente alla mappa di densità, il colore di un pixel rappresenta il valore atteso della variabile scelta
  
 plot(coastlines, add = T)
 points(covids)
@@ -284,8 +287,8 @@ points(covids)
 
 
 ##### mappa finale
-par(mfrow = c(2, 1)) # la schermata grafica viene suddivisa in due righe e una colonna
-                     # in questo modo si possono comparare la mappa di densità e di interpolazione del numero di casi
+par(mfrow = c(2, 1)) # FS  la schermata grafica viene suddivisa in due righe e una colonna
+                     # FS  in questo modo si possono comparare la mappa di densità e di interpolazione del numero di casi
  
 # densità
 cl5 <- colorRampPalette(c('cyan', 'purple', 'red')) (200) 
